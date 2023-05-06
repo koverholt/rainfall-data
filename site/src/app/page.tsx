e630024f902650cc08ff14c0e61ab3cae7517b14
Kristopher Overholt <koverholt@gmail.com>
2023-05-05 23:43:29 -0500

    Update

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -12,7 +12,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
-import { createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
@@ -100,183 +100,185 @@ function RainfallContent() {
   };
 
   return (
-    <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-      <CssBaseline />
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      >
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            Central Texas Rainfall Totals
+    <ThemeProvider theme={darkTheme}>
+      <React.Fragment>
+        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+        <CssBaseline />
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+        >
+          <Toolbar sx={{ flexWrap: 'wrap' }}>
+            <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+              Central Texas Rainfall Totals
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* Hero unit */}
+        <Container disableGutters maxWidth="lg" component="main" sx={{ pt: 3, pb: 3 }}>
+          <Typography variant="h6" align="left" color="text.secondary" component="p">
+            Showing rainfall from the Marble Falls 4 WSW LCRA Hydromet weather gauge
           </Typography>
-        </Toolbar>
-      </AppBar>
-      {/* Hero unit */}
-      <Container disableGutters maxWidth="lg" component="main" sx={{ pt: 3, pb: 3 }}>
-        <Typography variant="h6" align="left" color="text.secondary" component="p">
-          Showing rainfall from the Marble Falls 4 WSW LCRA Hydromet weather gauge
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      <Container maxWidth="lg" component="main">
-        <Grid container spacing={5} alignItems="flex-end" sx={{ pb: 4 }}>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["Since midnight"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      Today
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["5 day total"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      Last 5 days
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["This year"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      This year
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-        </Grid>
+        </Container>
+        {/* End hero unit */}
+        <Container maxWidth="lg" component="main">
+          <Grid container spacing={5} alignItems="flex-end" sx={{ pb: 4 }}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["Since midnight"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        Today
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["5 day total"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        Last 5 days
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["This year"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        This year
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
 
-        <Grid container spacing={5} alignItems="flex-end" sx={{ pb: 4 }}>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["1 day ago"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      Yesterday
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["30 day total"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      Last 30 days
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Card>
-                <CardHeader
-                  title={data ? data["rainfall_amounts"][0]["Last year"] + " in" : 'Loading...'}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
-                    <Typography component="h5" variant="h6" color="text.primary">
-                      Last year
-                    </Typography>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Grid>
-        </Grid>
-      </Container>
-      <Container sx={{ height: '250px', width: '800px', justifyContent: 'center' }}>
-        <Bar options={options} data={chartData} />
-      </Container>
-    </React.Fragment>
+          <Grid container spacing={5} alignItems="flex-end" sx={{ pb: 4 }}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["1 day ago"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        Yesterday
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["30 day total"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        Last 30 days
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={data ? data["rainfall_amounts"][0]["Last year"] + " in" : 'Loading...'}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{
+                      align: 'center',
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'baseline'}}>
+                      <Typography component="h5" variant="h6" color="text.primary">
+                        Last year
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
+        </Container>
+        <Container sx={{ height: '250px', width: '800px', justifyContent: 'center' }}>
+          <Bar options={options} data={chartData} />
+        </Container>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
 
